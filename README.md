e205009b9e6ecc63895ceb3a3f7d32a2eea57c70
lol <lol@lol.com>
2019-07-04 23:56:25 +1000

    a
    asdf

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 This project is the firmware for my z80 single board computer. The computer uses an
 ATMega32A as a bootloader, two blocks of 32kb ram (logic ic's doing bank switching).
 
-Code is provided as-is, good luck.
+code is provided as-is, good luck.
 docs/ folder contains datasheets and useful info
+z80asm / z80dasm work.
--- bootloader.c
+++ bootloader.c
@@ -9,6 +9,9 @@
 #define BAUDRATE 9600 // define baud
 #define BAUD_PRESCALER ((F_CPU)/(BAUDRATE * 16UL) - 1) // set baud rate value for UBRR
 
+#define TESTPROGRAM_LEN 12
+unsigned char TESTPROGRAM[12] = {0x3e,0xaa,0x00,0xf1,0x32,0x00,0xf1,0x3e,0xbb,0xc3,0x00,0x00};
+
 void uart_init (void)
 {
 	UBRRH = (uint8_t)(BAUD_PRESCALER>>8);
@@ -24,15 +27,15 @@ int uart_transmit(char data  )
 	return 0;
 }
 
-unsigned char uart_receive( void )
-{
-	while(!(UCSRA & (1<<RXC)));
+unsigned char uart_receive(void){
+	while(!(UCSRA & (1 << RXC)));
 	return UDR;
 }
 
 void setAddressBus(long int addr)
 {
-	PORTC = (addr >> 8) & 0x7F;
+	// top 4 bits reserved...
+	PORTC = (addr >> 8) & 0x0F;
 	PORTA = addr & 0xFF;
 }
 
@@ -50,16 +53,77 @@ void setAddressBus(long int addr)
 #define ASSERT_INT_LOW PORTC &= ~(1 << PORTC7)
 #define ASSERT_INT_HIGH PORTC |= (1 << PORTC7)
 
+#define ASSERT_BUSRQ_LOW PORTC &= ~(1 << PORTC5)
+#define ASSERT_BUSRQ_HIGH PORTC |= (1 << PORTC5)
+#define ASSERT_WAIT_LOW PORTC &= ~(1 << PORTC6)
+#define ASSERT_WAIT_HIGH PORTC |= (1 << PORTC6)
+
 #define ASSERT_DATABUS_INPUT DDRB = 0x00
 #define ASSERT_DATABUS_OUTPUT DDRB = 0xFF
 
+#define ASSERT_RWCTRL_OUTPUT DDRD |= (1 << PORTD4) | (1 << PORTD5)
+#define ASSERT_RWCTRL_INPUT DDRD &= ~((1 << PORTD4) | (1 << PORTD5))
+#define ASSERT_RWCTRL_PULLUPS PORTD |= (1 << PORTD4) | (1 << PORTD5)
+
+int grabNibble()
+{
+	unsigned char c = getchar();
+	if(c >= '0' && c <= '9')
+	{
+		return c - '0';
+	}
+	else if(c >= 'A' && c <= 'F')
+	{
+		return c - 'A' + 10;
+	}
+	else
+	{
+		return c - 'a' + 10;
+	}
+}
+
+int grabByte()
+{
+	return (grabNibble() << 4) + grabNibble();
+}
+
+void writeAndVerify(long int addr, int bytes, char *data)
+{
+	printf("%d to %04x\r\n",bytes,addr);
+	int i = 0;
+	ASSERT_DATABUS_OUTPUT;
+	ASSERT_MREQ_LOW;
+	ASSERT_RWCTRL_OUTPUT;
+	for(i = addr;i < bytes;i++)
+	{
+		setAddressBus(i);
+		PORTB = data[i];
+		ASSERT_WRITE_LOW;
+		ASSERT_WRITE_HIGH;
+	}
+	ASSERT_DATABUS_INPUT;
+	
+	unsigned char dataCheck;
+	for(i =addr;i < bytes;i++)
+	{
+		setAddressBus(i);
+		ASSERT_READ_LOW;
+		_delay_us(30);
+		dataCheck = PINB;
+		ASSERT_READ_HIGH;
+		if(dataCheck != data[i])
+		{
+			printf("ERR: addr %d byte %d\r\n",i,dataCheck);
+		}
+	}
+	ASSERT_MREQ_HIGH;
+	ASSERT_RWCTRL_INPUT;
+}
+
 int main(void)
 {
-	DDRC = 0xFF;
-	DDRA = 0xFF;
-	DDRB = 0xFF;
 	DDRD |= (1 << PORTD2) | (1 << PORTD3) | (1 << PORTD4) | (1 << PORTD5) | (1 << PORTD7);
-	
+	DDRD &= ~(1 << PORTD0);
 	// PORTD7 is ^MREQ (active low)
 	// PORTD6 is ^IOREQ
 	// PORTD5 is ^WRITE (active low)
@@ -68,58 +132,97 @@ int main(void)
 	// PORTD2 is ^RESET (active low)
 	// PORTD3 is CLK
 	
-	// PORTC7 is INT
+	// PORT C6 is WAIT (output to z80, active low)
+	// PORT C5 is BUSRQ (output to z80, active low)
+	// PORT C4 is BUSACK (input to atmega, active low)
+	// PORT C7 is INT (output to z80, active low)
 	// PORTA is LOW address
 	// PORTC is HIGH address
 	
+	ASSERT_WAIT_HIGH;
+	ASSERT_BUSRQ_LOW;
 	ASSERT_RESET_LOW;
-	ASSERT_CLK_LOW;
-	
+	// ASSERT_RESET_HIGH;
+	_delay_ms(1);
+	// let buses become tristated
+	DDRC = ~(1 << PORTC4);
+	DDRA = 0xFF;
+	DDRB = 0xFF;
 	uart_init ();
 	FILE mystdio = FDEV_SETUP_STREAM(uart_transmit, uart_receive, _FDEV_SETUP_RW);
 	stdout = &mystdio;
 	stdin = &mystdio;
 	
-	int i = 0;
-	printf("Boot OK, flashing virtual EEPROM...\n");
-	ASSERT_DATABUS_OUTPUT;
-	ASSERT_WRITE_LOW;
-	for(i = 0;i < 0xFF;i++)
+	ASSERT_RWCTRL_INPUT;
+	ASSERT_RWCTRL_PULLUPS;
+	
+	printf("EEPROM INIT\r\n");
+	
+	unsigned char c = 0;
+	int           load_pktlength = 0;
+	int           load_pkttype = 0;
+	long int      load_address = 0;
+	
+	while(1)
 	{
-		setAddressBus(i);
-		PORTB = 0x0;
-		ASSERT_MREQ_LOW;
-		_delay_ms(5);
-		ASSERT_MREQ_HIGH;
-		_delay_ms(5);
-	}
-	ASSERT_WRITE_HIGH;
-	printf("Verifying virtual EEPROM...\n");
-	ASSERT_DATABUS_INPUT;
-	ASSERT_READ_LOW;
-	unsigned char dataCheck;
-	for(i = 0;i < 0xFF;i++)
-	{
-		setAddressBus(i);
-		ASSERT_MREQ_LOW;
-		_delay_ms(5);
-		dataCheck = PINB;
-		if(dataCheck != 0x00)
+		c = getchar();
+		if(c == ':')
 		{
-			printf("ERR: addr %d byte %d\r\n",i,dataCheck);
+			load_pktlength = grabByte();
+			load_address   = (grabByte() << 8) + grabByte();
+			load_pkttype   = grabByte();
+			char pktArray[32];
+			if(load_pkttype == 0)
+			{
+				if(load_pktlength > 32)
+				{
+					printf("ERR:PKTLEN>32\r\n");
+					while(1)
+					{
+						_delay_ms(100);
+					}
+				}
+				int la = 0;
+				for(la = 0;la < load_pktlength;la++)
+				{
+					pktArray[la] = grabByte();
+				}
+				if(grabByte() != 0)
+				{
+					printf("ERR:CSUM<>0\r\n");
+					while(1)
+					{
+						_delay_ms(100);
+					}
+				}
+				writeAndVerify(load_address,load_pktlength,pktArray);
+			}
+			else if(load_pkttype == 1)
+			{
+				printf("SEND\r\n");
+				while(1)
+				{
+					_delay_ms(100);
+				}
+			}
+			else
+			{
+				printf("???\r\n");
+				while(1)
+				{
+					_delay_ms(100);
+				}
+			}
 		}
-		ASSERT_MREQ_HIGH;
-		_delay_ms(5);
 	}
-	ASSERT_READ_HIGH;
 	
-	setAddressBus(0);
-	PORTB = 0;
+	int i = 0;
 	
-	printf("Releasing pin control\r\n");
-	// all with pullups. source: https://cdn.hackaday.io/files/19000812896000/S221116_R100218_Z80.ino
-	DDRC = (1 << PORTC7);   // leave C7 for interrupt
-	PORTC = ~(1 << PORTC7);
+	// setAddressBus(0);
+	
+	printf("RELEASE CONTROL\r\n");
+	DDRC = (1 << PORTC7) | (1 << PORTC5) | (1 << PORTC6);
+	PORTC = 0xFF;
 	DDRA = 0x0;
 	PORTA = 0xFF;
 	DDRB = 0x0;
@@ -133,34 +236,42 @@ int main(void)
 	
 	_delay_ms(500);
 	
-	printf("Pulsing reset\r\n");
+	printf("PULSE RESET\r\n");
 	ASSERT_INT_HIGH;
+	ASSERT_BUSRQ_HIGH;
+	ASSERT_WAIT_HIGH;
 	ASSERT_RESET_LOW;
 	unsigned char addrBusLow = 0;
 	unsigned char addrBusHigh = 0;
 	unsigned char dataBus = 0;
 	unsigned char dataPin = 0;
-	
-	printf("Generating 4 clock cycles\r\n");
 	for(i = 0;i < 3; i++)
 	{
 		ASSERT_CLK_HIGH;
 		_delay_ms(50);
 		dataPin = PIND & (1 << PORTD7);
 		addrBusLow = PINA;
-		addrBusHigh = PINC & 0x7F;
+		addrBusHigh = PINC & 0x0F;
 		dataBus = PINB;
-		printf("%d %d %d : %d\r\n",dataPin,addrBusHigh,addrBusLow,dataBus);
+		if(dataPin == 0)
+		{
+			printf("%d %d %d : %d\r\n",dataPin,addrBusHigh,addrBusLow,dataBus);
+		}
 		ASSERT_CLK_LOW;
 		dataPin = PIND & (1 << PORTD7);
 		addrBusLow = PINA;
-		addrBusHigh = PINC & 0x7F;
+		addrBusHigh = PINC & 0x0F;
 		dataBus = PINB;
-		printf("%d %d %d : %d\r\n",dataPin,addrBusHigh,addrBusLow,dataBus);
+		if(dataPin == 0)
+		{
+			printf("%d %d %d : %d\r\n",dataPin,addrBusHigh,addrBusLow,dataBus);
+		}
 		_delay_ms(50);
 	}
 	ASSERT_RESET_HIGH;
-	printf("It's alive!\r\n");
+	printf("GO!\r\n");
+	
+	_delay_ms(1000);
 
 	while(1)
 	{
@@ -168,15 +279,21 @@ int main(void)
 		_delay_ms(50);
 		dataPin = PIND & (1 << PORTD7);
 		addrBusLow = PINA;
-		addrBusHigh = PINC & 0x7F;
+		addrBusHigh = PINC & 0x0F;
 		dataBus = PINB;
-		printf("%d %d %d : %d\r\n",dataPin,addrBusHigh,addrBusLow,dataBus);
+		if(dataPin == 0)
+		{
+			printf("%d %d %d : %d\r\n",dataPin,addrBusHigh,addrBusLow,dataBus);
+		}
 		ASSERT_CLK_LOW;
 		dataPin = PIND & (1 << PORTD7);
 		addrBusLow = PINA;
-		addrBusHigh = PINC & 0x7F;
+		addrBusHigh = PINC & 0x0F;
 		dataBus = PINB;
-		printf("%d %d %d : %d\r\n",dataPin,addrBusHigh,addrBusLow,dataBus);
+		if(dataPin == 0)
+		{
+			printf("%d %d %d : %d\r\n",dataPin,addrBusHigh,addrBusLow,dataBus);
+		}
 		_delay_ms(50);
 	}
 }
